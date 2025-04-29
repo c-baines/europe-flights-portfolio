3f03d96394c6a99c324865b8b70c32c7337a7e32
Chalissar Baines <88324631+c-baines@users.noreply.github.com>
2025-04-29 17:15:53 +0100

    upaate ingestion scripts for emissions data

--- setup/data_ingestion.py
+++ setup/data_ingestion.py
@@ -10,12 +10,23 @@ c-baines
 import pandas as pd
 from pathlib import Path
 import os
-from src.db import FlightList, session
+from src.db import FlightList, Emissions, TableName, session
 from loguru import logger
 
 here = Path(__file__).resolve().parent 
 
-def ingest_flight_list_csv(filename: str):
+def dict_to_db(row: dict, table: TableName):
+    if table == TableName.flight_list:
+        row.update({'ec_id': row['id']})
+        row.pop('id')
+        db_obj = FlightList(**row)
+    if table == TableName.emissions:
+        # make key name lower case 
+        row = {k.lower(): v for k,v in row.items()}
+        db_obj = Emissions(**row)
+    return db_obj
+
+def ingest_csv(filename: str, table: TableName):
     """
     - Reads .csv file
     - For each row: 
@@ -30,9 +41,7 @@ def ingest_flight_list_csv(filename: str):
     df = pd.read_csv(filename)
     db_objects = []
     for row in df.to_dict(orient='records'): 
-        row.update({'ec_id': row['id']})
-        row.pop('id')
-        db_obj = FlightList(**row)
+        db_obj = dict_to_db(row, table)
         db_objects.append(db_obj)
         if len(db_objects) == 100000:
             session.bulk_save_objects(db_objects)
@@ -61,20 +70,19 @@ def iterate_folder(folder: str):
             if file.endswith('.csv'):
                 yield os.path.join(root, file)
 
-
-def ingest_flight_list_folder():
+def ingest_folder(folder: str, table: TableName):
     """ 
-    - Iterates through flight_list folder
-    - Calls ingest_flight_list_csv on each file 
+    - Iterates through folder
+    - Calls ingest_csv on each file 
     """
-    for filename in iterate_folder(str(here/'data'/'flight_list')):
+    for filename in iterate_folder(str(here/'data'/folder)):
         # skip files already ingested
-        if "flight_list_2022" in filename:
+        if "flight_list_2022" in filename or "flight_list_2025" in filename:
             logger.info(f"Skipping {filename}")
             continue
         logger.info(f"Processing {filename}")
-        ingest_flight_list_csv(filename)
+        ingest_csv(filename, table)
         logger.info(f"Finished processing {filename}")
 
-
-ingest_flight_list_folder()
+# ingest_folder('flight_list', TableName.flight_list)
+# ingest_folder('co2_emissions_by_state', TableName.emissions)
--- src/db.py
+++ src/db.py
@@ -1,7 +1,7 @@
 """
 db.py 
 
-Initialise postgreSQL database with flight_list variables
+Initialise postgreSQL database with variables for flight_list, co2_emissions_by_state
 
 1. Connect to local postgreSQL 
 2. Define table class with variables 
@@ -12,12 +12,13 @@ c-baines
 """
 
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+# from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.engine import URL
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, Float, Boolean
 from dotenv import load_dotenv, find_dotenv
 import os
+from enum import Enum
 
 load_dotenv(find_dotenv())
 
@@ -32,7 +33,7 @@ url = URL.create(
     port="5432",
 )
 
-# creating the db session for querying and add entries in tables
+# create the db session for querying and add entries in tables
 engine = create_engine(url=url) 
 session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 session = session()
@@ -90,5 +91,39 @@ class FlightList(Base):
     version = Column(String, nullable=True)
     unix_time = Column(Integer, nullable=True)
 
-# Create just the "flight_list" table, run this the first time 
+class Emissions(Base):
+    """
+    Represents emissions table in the db
+    
+    Attributes:
+        __tablename__ (str): PostgreSQL table name 
+        __table_args__ (dict): Additional table arguments
+        year (int): year of record
+        month (int): number of month of record
+        state_name (str): name of state
+        state_code (str): 2 letter state abbreviation
+        co2_qty_tonnes (flt): quantitity of CO2 emissions in tonnes (kg)
+        tf (int): traffic for the state 
+        note (bool): special aggregation for state
+    """
+
+    __tablename__ = "emissions"
+    __table_args__ = {"schema": "public"}
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    year = Column(Integer)
+    month = Column(Integer)
+    state_name = Column(String)
+    state_code = Column(String)
+    co2_qty_tonnes = Column(Float)
+    tf = Column(Integer)
+    note = Column(Boolean)
+
+class TableName(Enum):
+    emissions = 'emissions'
+    flight_list = 'flight_list'
+
+
+# Create just the "flight_list"/"co2_emissions" table, run this the first time 
 # Base.metadata.tables['public.flight_list'].create(engine)
+# Base.metadata.tables['public.emissions'].create(engine)
