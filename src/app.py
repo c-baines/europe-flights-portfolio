7d387babf31555791d95099f243a85bc9b9ca743
Chalissar Baines <88324631+c-baines@users.noreply.github.com>
2025-08-19 18:10:16 +0100

    update app styling

--- src/app.py
+++ src/app.py
@@ -3,16 +3,17 @@ src/app.py
 
 author: c-baines
 created: 28/4/25
-modified: 9/7/25
+modified: 5/8/25
 """
 
 from dash import Dash
 from src.layout import layout
 from src.callbacks import register_callbacks
 import os
+import dash_bootstrap_components as dbc
 
 assets_path = os.path.join(os.path.dirname(__file__), '..', 'assets')
-app = Dash(__name__, assets_folder=assets_path)
+app = Dash(__name__, assets_folder=assets_path, external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.layout = layout
 register_callbacks(app)
 
--- src/callbacks.py
+++ src/callbacks.py
@@ -2,7 +2,7 @@
 src/callbacks.py
 
 created: 19/5/25
-modified: 14/7/25
+modified: 19/8/25
 """
 from dash import Input, Output, callback
 from src.queries import STARTUP_QUERIES, get_counts_cards, get_top_airlines, get_top_models
@@ -11,14 +11,16 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+import pandas as pd
 
 
 
 def register_callbacks(app): 
     """
-    
+    Registers all Dash callbacks with the passed app instance. This function prevents circular imports by centralizing callback registration.
+
     Args:
-        app ():
+        app (dash.Dash): The Dash app instance to which the callbacks will be registered.
 
     """
     # Flight counts line graph
@@ -34,7 +36,7 @@ def register_callbacks(app):
 
         Args:
             month_string (str): a string representing the selected month (e.g. "January 2025")
-                If 'all' is selected, no filtering is applied and all months are included.
+                If 'all' is selected no filtering is applied and all months are included.
 
         Returns:
             plotly.graph_objects.Figure: a Plotly figure containing the time series line chart.
@@ -52,13 +54,13 @@ def register_callbacks(app):
             'overflights': '#F46A25'
         }
 
-        mode_size_map = {
-            'total': 8,
-            'intra-eu': 6,
-            'arrivals_from_outside': 6,
-            'departures_to_outside': 6,
-            'overflights': 6
-        }
+        # mode_size_map = {
+        #     'total': 8,
+        #     'intra-eu': 6,
+        #     'arrivals_from_outside': 6,
+        #     'departures_to_outside': 6,
+        #     'overflights': 6
+        # }
 
         label_map = {
             'total': 'Total',
@@ -67,6 +69,7 @@ def register_callbacks(app):
             'departures_to_outside': 'Departures',
             'overflights': 'Overflights'
         }
+
         fig = go.Figure()
 
         for category in df['category'].unique():
@@ -77,9 +80,9 @@ def register_callbacks(app):
                     x=category_df['dof'],
                     y=category_df['count'],
                     mode='lines',
-                    name=label_map.get(category, category), # gets category key value from dict or default category label 
+                    name=label_map.get(category, category), # gets category key value from dict, or default category label 
                     line=dict(color=color_map[category]),
-                showlegend=True
+                    showlegend=True
                 )
             )
 
@@ -133,7 +136,8 @@ def register_callbacks(app):
             ),
             plot_bgcolor='white', 
             height=500,
-            template="plotly_white"
+            template="plotly_white",
+            margin=dict(t=40)
         )
 
         fig.update_layout(
@@ -177,51 +181,59 @@ def register_callbacks(app):
             if prev_df.empty:
                 vals = {col:(curr_df[col].to_list()[0], None) for col in columns}
             else:
-                vals = {col:(curr_df[col].to_list()[0], prev_df[col].to_list()[0]) for col in columns}
-
-        # fig = make_subplots(
-        #         rows=1, cols=len(columns),
-        #         specs=[[{'type': 'domain'}]*len(columns)]
-        #     )
-        
-        # count=1 # count is for tracking column position [0, 1, 2, 3, ...]
-        # for col, val in vals.items():
-        #     fig.add_trace(
-        #         go.Indicator(
-        #             mode="number+delta",
-        #             value=val[0],
-        #             delta={'reference': val[1], 'relative': True, 'valueformat': '.1%'} if val[1] else None, 
-        #             title={'text': col.replace('_', ' ').title()}),
-        #             row=1, col=count
-        #     )
-        #     count+=1
-
-        # fig.update_layout(
-        #     paper_bgcolor = "lightgray",
-        #     height=250)
-
-        items = list(vals.items())
-        items[2], items[3] = items[3], items[2] # switched positions of cards
+                vals = {col:(curr_df[col].to_list()[0], prev_df[col].to_list()[0]) for col in columns} 
 
         fig = make_subplots(
-            rows=2, cols=2,
-            specs=[[{'type': 'domain'}, {'type': 'domain'}],
-                [{'type': 'domain'}, {'type': 'domain'}]]
-        )
-
-        for i, (col, val) in enumerate(vals.items()):
-            row = i // 2 + 1   # row 1 or 2
-            col_num = i % 2 + 1  # col 1 or 2
+                rows=1, cols=len(columns),
+                specs=[[{'type': 'domain'}]*len(columns)]
+            )
+        
+        count=1 # count is for tracking column position [0, 1, 2, 3, ...]
+        for col, val in vals.items():
             fig.add_trace(
                 go.Indicator(
                     mode="number+delta",
                     value=val[0],
                     delta={'reference': val[1], 'relative': True, 'valueformat': '.1%'} if val[1] else None, 
-                    title={'text': col.replace('_', ' ').title()}
+                    title={'text': col.replace('_', ' ').title()},
+                    number={"font": {"size": 60}},
+                    domain={'x': [0.5, 0.95], 'y': [0.5, 0.95]}
                 ),
-                row=row, col=col_num
+                    row=1, col=count
             )
+            count+=1
 
+        fig.update_layout(
+            height=200,
+            margin=dict(t=30, b=10, l=10, r=10)  # reduce outer padding
+        )
+
+        # items = list(vals.items())
+        # items[2], items[3] = items[3], items[2] # switched positions of cards
+
+        # fig = make_subplots(
+        #     rows=1, cols=4,
+        #     specs=[[{'type': 'domain'}, {'type': 'domain'}],
+        #         [{'type': 'domain'}, {'type': 'domain'}]]
+        # )
+
+        # for i in range(1,5):
+        #     fig.add_trace(
+        #         go.Indicator(
+        #             more="number+delta",
+        #             value=
+        #         )
+        #     )
+        # for i, (col, val) in enumerate(vals.items()):
+        #     fig.add_trace(
+        #         go.Indicator(
+        #             mode="number+delta",
+        #             value=val[0],
+        #             delta={'reference': val[1], 'relative': True, 'valueformat': '.1%'} if val[1] else None, 
+        #             title={'text': col.replace('_', ' ').title()}
+        #         ),
+        #         col=i
+        #     )
 
         return fig
   
@@ -264,58 +276,76 @@ def register_callbacks(app):
         x.reverse()
         y.reverse()
 
-        fig = make_subplots(
-            rows=1, cols=2,
-            # specify type of figure. px.choropleth not automatically compatible with make_subplots()
-            specs=[[{'type': 'choropleth'}, {'type': 'xy'}]],
-            column_widths=[0.6, 0.4]
-        )
+        choro_fig = go.Figure(go.Choropleth(
+            locations=df['iso_alpha3'],           
+            z=df['co2_qty_tonnes'],              
+            text=df['state_name'],                
+            colorscale='Blues',
+            marker_line_color='white',           
+            marker_line_width=1.5,                
+            colorbar_title='CO₂ (tonnes)'
+        ))
 
-        fig.add_trace(
-            px.choropleth(
-                    df,
-                    locations='iso_alpha3',
-                    color='co2_qty_tonnes',
-                    hover_name='state_name',
-                    projection='natural earth',
-                    ).data[0],
-            row=1, col=1
-        )
-
-        fig.add_trace(
-            go.Bar(
-                x = x,
-                y = y,
-                marker=dict(
-                    color='rgba(50, 171, 96, 0.6)',
-                    line=dict(
-                    color='rgba(50, 171, 96, 1.0)',
-                    width=1)
-                ),
-                name='CO2 quantity (tonnes)',
-                orientation='h'
+        choro_fig.update_layout(
+            title='CO₂ Emissions by Country',
+            geo=dict(
+                showframe=False,
+                showcoastlines=True,
+                showcountries=True,
+                projection_type='natural earth',
+                bgcolor='white',  
+                center=dict(lat=52, lon=10),
+                projection_scale=2.5
             ),
-            row=1, col=2
+            height=600,
+            margin=dict(l=0, r=0, t=50, b=0),
+            font=dict(
+                family='Open Sans, sans-serif',
+                size=14,
+                color='#2a3f5f'
+            )
         )
 
-        fig.update_geos(
-            projection_type="natural earth",
-            showcoastlines=True,
-            showframe=False,
-            row=1, col=1
-        )
+        # fig = make_subplots(
+        #     rows=1, cols=2,
+        #     # specify type of figure. px.choropleth not automatically compatible with make_subplots()
+        #     specs=[[{'type': 'choropleth'}, {'type': 'xy'}]],
+        #     column_widths=[0.7, 0.3]
+        # )
+        # # bar graph
+        # fig.add_trace(
+        #     go.Bar(
+        #         x = x,
+        #         y = y,
+        #         marker=dict(
+        #             color='rgba(50, 171, 96, 0.6)',
+        #             line=dict(
+        #             color='rgba(50, 171, 96, 1.0)',
+        #             width=1)
+        #         ),
+        #         name='CO2 quantity (tonnes)',
+        #         orientation='h'
+        #     ),
+        #     row=1, col=2
+        # )
 
-        fig.update_layout(
-            title='CO₂ Emissions and Top Emitters',
-            height=600
-        )
+        # fig.update_geos(
+        #     projection_type="natural earth",
+        #     showcoastlines=True,
+        #     showframe=False,
+        #     row=1, col=1
+        # )
+
+        # fig.update_layout(
+        #     title='CO₂ Emissions and Top Emitters',
+        #     height=600
+        # )
+        
+        return choro_fig
     
-        return fig
-
-    # Airlines and aircraft plots
+    # Airlines and aircraft pie and bar graphs
     @app.callback(
         Output('airlines-bar-graph', 'figure'),
-        # Output('manufacturer-pie-chart', 'figure'),
         Input('airlines-dropdown', 'value')
     )    
     def update_aircraft_graphs(year):
@@ -334,7 +364,8 @@ def register_callbacks(app):
         airlines_df['airline'] = airlines_df['airline'].replace({
             "TURK HAVA YOLLARI (TURKISH AIRLINES CO.)": "TURKISH<br>AIRLINES",
             "DEUTSCHE LUFTHANSA, AG, KOELN": "LUFTHANSA",
-            "EASYJET UK LTD": "EASYJET"
+            "EASYJET UK LTD": "EASYJET",
+            "BRITISH AIRWAYS": "BRITISH<br>AIRWAYS"
         }) 
 
         if year!='all':
@@ -347,6 +378,7 @@ def register_callbacks(app):
             model_df = model_df.head(10)
 
             manufacturer_df = manufacturer_df[manufacturer_df['year']==year]
+            manufacturer_df.sort_values('year', ascending=False, inplace=True)
 
         else:
             airlines_df = airlines_df.groupby(['airline']).agg({
@@ -377,23 +409,53 @@ def register_callbacks(app):
 
         fig = make_subplots(
             rows=1, cols=3,
-            subplot_titles=('Market Share', 'Top Aircraft Models', 'Top Airlines'),
-            specs=[[{'type':'domain'}, {'type':'bar'}, {'type': 'bar'}]]
+            subplot_titles=('Manufacturer Share', 'Top Aircraft Models', 'Top Airlines'),
+            specs=[[{'type':'domain'}, {'type':'bar'}, {'type': 'bar'}]],
+            shared_xaxes=True,
+            column_widths=[0.3, 0.35, 0.35] # must add to 1
         )
 
         total = manufacturer_df['count'].sum()
         percentages = manufacturer_df['count'] / total * 100
-       
+
+        color_map = {'Airbus': '#12436D', 
+                     'Bombardier': '#FFC000', 
+                     'Cessna': '#3D3D3D',
+                     'ATR': '#A285D1',
+                     'Embraer': '#801650',
+                     'Boeing': '#28A197',
+                     'Other':'#F46A25',
+                     'Piper': '#2073BC'
+        }
+        
         fig.add_trace(
             go.Pie(
             labels=manufacturer_df['manufacturer'],
             values=manufacturer_df['count'],
             textinfo='label+percent',
-            textposition=['inside' if pct >= 4 else 'outside' for pct in percentages],
-            name=''
-            ),
+            # textposition=['inside' if pct >= 3.5 else 'outside' for pct in percentages],
+            textposition='auto',
+            name='',
+            marker=dict(colors=[color_map[m] for m in manufacturer_df['manufacturer']]),
+            domain=dict(x=[0, 1], y=[0, 0.85])  # shrink vertically
+            ),            
             row=1, col=1
         )
+
+        # for category in df['category'].unique():
+        #     category_df = df[df['category'] == category]
+            
+        #     fig.add_trace(
+        #         go.Scatter(
+        #             x=category_df['dof'],
+        #             y=category_df['count'],
+        #             mode='lines',
+        #             name=label_map.get(category, category), # gets category key value from dict or default category label 
+        #             line=dict(color=color_map[category]),
+        #             showlegend=True
+        #         )
+        #     )
+
 
         fig.add_trace(
             go.Bar(
@@ -453,8 +515,162 @@ def register_callbacks(app):
 
         fig.update_layout(
             plot_bgcolor='white',
-            showlegend=False
+            showlegend=False,
+            height=450,
+            yaxis2=dict(domain=[0.0, 1.0]),  # bar chart 1
+            yaxis3=dict(domain=[0.0, 1.0]),
+            margin=dict(t=50),
+            # autosize=False 
+        )
+    
+        return fig     
+
+    @app.callback(
+        Output('manufacturer-percent-graph', 'figure'),
+        Input('airlines-dropdown', 'value')
+    )    
+    def update_manufacturer_percent(year):
+        """
+
+        Args:
+            year ():
+
+        Returns:
+            plotly.graph_objects.Figure: 
+        """
+        df = STARTUP_QUERIES.MANUFACTURER_PERCENT_DF.copy()
+
+        if year!='all':
+            df = df[df['year']==year]
+
+        fig = go.Figure()
+
+        color_map = {'Airbus': '#12436D', 
+                    'Bombardier': '#FFC000',
+                    'Cessna': '#3D3D3D',
+                    'ATR': '#A285D1',
+                    'Embraer': '#801650',
+                    'Boeing': '#28A197',
+                    'Other':'#F46A25',
+                    'Piper': '#2073BC'
+        }
+
+        # want this plot to show manufacturer count by month and year 
+        for manufacturer in df['manufacturer'].unique():
+            df_subset = df[df['manufacturer'] == manufacturer]
+
+            # want to show them as a share of the total 
+
+            fig.add_trace(go.Scatter(
+                x=df_subset['date'], 
+                y=df_subset['percentage'],
+                mode='lines',
+                name=manufacturer,
+                marker=dict(color=color_map.get(manufacturer, 'rgb(204, 204, 204)'))
+            ))
+
+        fig.update_layout(
+            xaxis=dict(
+                showgrid=False,
+                showline=True,   
+                linecolor='rgb(204, 204, 204)',
+                linewidth=2,
+                tickmode='linear',
+                dtick=1,              
+                tickformat='d',
+                ticklen=5,
+                ticks='outside',
+                tickwidth=2,
+                tickcolor='rgb(204, 204, 204)'
+            ),
+            yaxis=dict(
+                showgrid=True,
+                showline=False,   
+                linecolor='rgb(204, 204, 204)',
+                linewidth=2,
+                griddash='dot',
+                gridcolor='rgb(204, 204, 204)'       
+            ),
+            legend=dict(
+                orientation="h",     
+                yanchor="bottom",
+                y=-0.25,              
+                xanchor="center",
+                x=0.5
+            ),
+            title="Percentage Share",
+            xaxis_title="Year",
+            yaxis_title="Percent",
+            template="plotly_white",
+            plot_bgcolor='white', 
+            height=500
+        )
+
+        fig.update_xaxes(
+            dtick="M6",  # show every 6 months 
+            tickformat="%b %Y"  # abbreviated month name + year (Jan 2021, Feb 2021...)
         )
 
         return fig
 
+    # @app.callback(
+    #     Output(), 
+    #     Input()
+    # )
+    # def update_emissions_bar():
+    #     """
+        
+    #     """
+        # emissions_df = STARTUP_QUERIES.COUNTRY_EMISSIONS_DF
+        # emissions_df['date'] = pd.to_datetime(emissions_df[['year', 'month']].assign(day=1))
+
+        # line_fig = go.Figure()
+
+        # for state in emissions_df['state_name'].unique():
+        #     state_df = emissions_df[emissions_df['state_name'] == state]
+            
+        #     line_fig.add_trace(
+        #         go.Scatter(
+        #             x=state_df['date'], # x should be date 
+        #             y=state_df['co2_qty_tonnes'],
+        #             mode='lines',
+        #             name=state,
+        #             line=dict(color='rgb(192, 192, 192)')
+        #         )
+        #     )
+
+        # line_fig.update_layout(
+        #     xaxis=dict(
+        #         showgrid=False,  
+        #         showline=True,   
+        #         linecolor='rgb(204, 204, 204)',
+        #         linewidth=2,
+        #         ticklen=5,
+        #         ticks='outside',
+        #         tickwidth=2,
+        #         tickcolor='rgb(204, 204, 204)'
+        #     ),
+        #     yaxis=dict(
+        #         showgrid=True, 
+        #         griddash='dot',
+        #         showline=False,   
+        #         linecolor='rgb(204, 204, 204)',
+        #         linewidth=2
+        #     ),
+        #     plot_bgcolor='white', 
+        #     height=500,
+        #     template="plotly_white",
+        #     showlegend=False
+        # )
+
+        # avg_emissions_df = emissions_df.groupby('date')['co2_qty_tonnes'].mean().reset_index()
+        # avg_emissions_df.rename(columns={'co2_qty_tonnes': 'avg_co2_qty_tonnes'}, inplace=True)
+
+        # line_fig.add_trace(
+        #     go.Scatter(
+        #         x=avg_emissions_df['date'],
+        #         y=avg_emissions_df['avg_co2_qty_tonnes'],
+        #         mode='lines',
+        #         line=dict(color='red', width=4) 
+        #     )
+        # )
