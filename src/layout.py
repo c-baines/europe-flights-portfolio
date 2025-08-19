ecb3f4a64896b441db0a59870626371bd4ba463a
Chalissar Baines <88324631+c-baines@users.noreply.github.com>
2025-08-19 18:12:04 +0100

    update layout and add navbar

--- src/layout.py
+++ src/layout.py
@@ -1,10 +1,10 @@
 """
 src/layout.py
 
-App layout
+App html layout
 
 created: 19/5/25
-modified: 14/7/25
+modified: 19/8/25
 """
 
 from dash import html, dcc, Input, Output
@@ -12,112 +12,98 @@ import pandas as pd
 import plotly.express as px
 from src.queries import get_flight_counts_by_day, get_months_unique, get_country_emissions, get_year_emissions, get_month_emissions, STARTUP_QUERIES
 import plotly.graph_objects as go
+from statistics import mean
+import dash_bootstrap_components as dbc
 
 
-manufacturer_df = STARTUP_QUERIES.MANUFACTURER_COUNTS_DF.copy()
-manu_fig = go.Figure()
+navbar = dbc.NavbarSimple(
+    brand="Europe Flights Portfolio Project",
+    color="#12436D",
+    dark=True,
+    fluid=True,    
+    style={"padding-top": "20px", "padding-bottom": "20px"}  
 
-for manufacturer in manufacturer_df['manufacturer'].unique():
-    df_subset = manufacturer_df[manufacturer_df['manufacturer'] == manufacturer]
-
-    manu_fig.add_trace(go.Scatter(
-        x=df_subset['year'],
-        y=df_subset['count'],
-        mode='lines',
-        name=manufacturer
-    ))
-
-manu_fig.update_layout(
-    xaxis=dict(
-        showgrid=False,
-        showline=True,   
-        linecolor='rgb(204, 204, 204)',
-        linewidth=2,
-        tickmode='linear',
-        dtick=1,              
-        tickformat='d',
-        ticklen=5,
-        ticks='outside',
-        tickwidth=2,
-        tickcolor='rgb(204, 204, 204)'
-    ),
-    yaxis=dict(
-        showgrid=True,
-        showline=False,   
-        linecolor='rgb(204, 204, 204)',
-        linewidth=2,
-        griddash='dot',
-        gridcolor='rgb(204, 204, 204)'       
-    ),
-    legend=dict(
-        orientation="h",     
-        yanchor="bottom",
-        y=-0.25,              
-        xanchor="center",
-        x=0.5
-    ),
-    title="Number of Recorded Flights by Manfactuerer",
-    xaxis_title="Year",
-    yaxis_title="Number of Aircraft",
-    template="plotly_white",
-    plot_bgcolor='white', 
-    height=500
 )
 
 layout = html.Div([
-    # number of flights line graph
-    html.Div(children=[
-        html.H1("European Flight Records Dashboard"),
+    navbar,
+
+    # Flights overview container
+    html.Div([
+        html.H3("Flight History"),
 
         dcc.Dropdown(
-            id='flight-count-dropdown', 
+            id='flight-count-dropdown',
             options=[{"label": "All data", "value": "all"}] + [{"label": m, "value": m} for m in get_months_unique()],
             value="all",
-            placeholder='Select a Month'
+            clearable=False,
+            className='my-dropdown'
         ),
 
-    # flight count decomposed cards
         dcc.Graph(id="card"),
-
         dcc.Graph(id='flight-count-graph')
-    ]),
+    ], style={
+        "backgroundColor": "white",
+        "borderRadius": "12px",
+        "padding": "20px",
+        "margin": "20px",
+        "boxShadow": "0 2px 6px rgba(0,0,0,0.15)"
+    }),
 
-    # airlines bar graphs
-    html.Div(children=[
-        html.H2('Airlines and Aircraft'),
+    # Airlines + manufacturers container
+    html.Div([
+        html.H3('Airlines and Aircraft'),
 
         dcc.Dropdown(
             id='airlines-dropdown',
-            options=[{"label": "All years", "value": "all"}] + [{"label": y, "value": y} for y in STARTUP_QUERIES.TOP_AIRLINES_DF['year'].unique().tolist()],
+            options=[{"label": "All years", "value": "all"}] + [{"label": y, "value": y} for y in sorted(STARTUP_QUERIES.TOP_AIRLINES_DF['year'].unique())],
             value='all',
-            placeholder='Year' 
+            clearable=False,
+            className='my-dropdown'
         ),
 
         dcc.Graph(id='airlines-bar-graph'),
+        dcc.Graph(id='manufacturer-percent-graph')
+    ], style={
+        "backgroundColor": "white",
+        "borderRadius": "12px",
+        "padding": "20px",
+        "margin": "20px",
+        "boxShadow": "0 2px 6px rgba(0,0,0,0.15)"
+    }),
 
-        dcc.Graph(id='manufacturer-line-graph', figure=manu_fig)
-    ]),
+    # Emissions container
+    html.Div([
+        html.H3('Flight Emissions'),
 
-    # emissions heatmap
-    html.Div(children=[
-        html.H1('Choropleth Emissions Test'),
-        
-        dcc.Dropdown(
-            id='choropleth-dropdown-year',
-            options=[{"label": "All years", "value": "all"}] + [{"label": y, "value": y} for y in get_year_emissions()],
-            value="all",
-            placeholder='Year'
-        ),
+        html.Div([
+            html.Div([
+                dcc.Dropdown(
+                    id='choropleth-dropdown-year',
+                    options=[{"label": "All years", "value": "all"}] + [{"label": y, "value": y} for y in get_year_emissions()],
+                    value="all",
+                    clearable=False,
+                    className='my-dropdown'
+                )
+            ], className="dropdown-container"),
 
-        dcc.Dropdown(
-            id='choropleth-dropdown-month',
-            options=[{"label": "All months", "value": "all"}] + [{"label": m, "value": m} for m in get_month_emissions()],
-            value="all",
-            placeholder='Month'
-        ),
+            html.Div([
+                dcc.Dropdown(
+                    id='choropleth-dropdown-month',
+                    options=[{"label": "All months", "value": "all"}] + [{"label": m, "value": m} for m in get_month_emissions()],
+                    value="all",
+                    clearable=False,
+                    className='my-dropdown'
+                )
+            ], className="dropdown-container")
+        ]),
 
         dcc.Graph(id='emissions-choropleth')
-    ])
-
+    ], style={
+        "backgroundColor": "white",
+        "borderRadius": "12px",
+        "padding": "20px",
+        "margin": "40px",
+        "boxShadow": "0 2px 6px rgba(0,0,0,0.15)"
+    })
 ])
-
