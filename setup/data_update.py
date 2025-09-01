d038f9077ab67e83478900db61809ad12eac313f
Chalissar Baines <chalissar.baines@gmail.com>
2025-09-01 21:31:54 +0100

    update

--- setup/data_update.py
+++ setup/data_update.py
@@ -4,16 +4,25 @@ setup/data_update.py
 Downloads and ingests new data releases and updates PostgreSQL DB. 
 
 created: 26/7/25
-modified: 4/8/25
+modified: 29/8/25
 """
 
 from setup import data_ingestion as ingest, data_download as download
+from setup.data_download import download_metadata 
+from src.db import create_tables
 
 # download data 
-download.update()
+# download_metadata()
 
 # ingest data
-ingest.update() # want to speed this up - far too slow
+# create_tables() # create empty DB tables
+# ingest.setup() # ingset all data
 
-# update materialized views 
+
+# update data 
+# download.update() # download new data releases 
+# ingest.update() # ingest new data releases 
+
+
+
 
